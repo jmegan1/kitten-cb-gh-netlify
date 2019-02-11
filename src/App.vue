ac0360e6cb1a5882a76b1f86f3ffe7a9fb065444
jeremiah egan <37743535+jmegan1@users.noreply.github.com>
2019-02-11 15:19:28 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img width="25%" :src="avatar" /> {{ message }}
+    <button @click="sayHello">click me sucka</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      message: " this is super duper fun working in vue",
+      avatar: "http://placekitten.com/g/300/300"
+    };
+  },
+  methods: {
+    sayHello() {
+      alert("Vue.js is crazy fun");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: pink;
+  background-color: grey;
+  margin-top: 60px;
+}
+</style>
