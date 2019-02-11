82784ba1440a44f523b699beccf0ee14d235b68e
jeremiah egan <37743535+jmegan1@users.noreply.github.com>
2019-02-11 15:29:39 -0700

    added words to site
    im super cool man

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@ export default {
   name: "App",
   data() {
     return {
-      message: " this is super duper fun working with vue",
+      message:
+        " this is super duper fun working with vue.....codebox,github, and netlify is a great workflow choice",
       avatar: "http://placekitten.com/g/300/300"
     };
   },
